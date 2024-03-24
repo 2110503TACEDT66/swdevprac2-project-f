73a77e3a7d056fc7a38c318a2cc09f405e653134
whale <Whale@DESKTOP-8C9QGHK.(none)>
2024-03-24 22:37:54 +0700

    reservationlist

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -1,57 +1,52 @@
-"use client"
 
-import { removeBooking } from "@/redux/features/bookSlice"
-import { AppDispatch, useAppSelector } from "@/redux/store"
-import { useDispatch } from "react-redux"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import getUserProfile from "@/libs/getUserProfile"
+import { getServerSession } from "next-auth/next"
 
-export default function BookingList() {
-    const bookingItem = useAppSelector((state) => state.bookSlice.bookItems);
-    const dispatch = useDispatch<AppDispatch>();
+export default async function BookingList() {
+    const months: string[] = [
+        "January", "February", "March", "April", "May", "June", "July", 
+        "August", "September", "October", "November", "December"
+    ];
+    
+    const session = await getServerSession(authOptions)
+    if(!session||!session.user.token) return null
+  
+    const profile = await getUserProfile(session.user.token)
+    const time = profile.data.createdAt
+    const dateObj: Date = new Date(time);
+    const day: string = ("0" + dateObj.getDate()).slice(-2);
+    const monthIndex: number = dateObj.getMonth();
+    const month: string = months[monthIndex];
+    const year: number = dateObj.getFullYear();
+    
+    const formattedDate: string = `${day} ${month} ${year}`;
 
     return (
         <>
-            {bookingItem.length > 0 ? (
-                bookingItem.map((item) => (
-                    <div className="bg-slate-200 rounded-px mx-5 py-2 my-2 text-black" key={item.id}>
-                        <table className='table-auto border-separate border-spacing-2 bg-gray-100'>
-                            <tbody>
-                                <tr>
-                                    <td>FirstName</td>
-                                    <td>{item.name}</td>
-                                </tr>
-                                <tr>
-                                    <td>LastName</td>
-                                    <td>{item.surname}</td>
-                                </tr>
-                                <tr>
-                                    <td>Citizen-ID</td>
-                                    <td>{item.id}</td>
-                                </tr>
-                                <tr>
-                                    <td>Hospital</td>
-                                    <td>{item.hospital}</td>
-                                </tr>
-                                <tr>
-                                    <td>Date</td>
-                                    <td>{item.bookDate}</td>
-                                </tr>
-                                <tr>
-                                    <td colSpan={2}>
-                                    <button
-                                        className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-indigo-400"
-                                        onClick={() => { dispatch(removeBooking(item.id)) }} name='remove'>
-                                        remove
-                                    </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        
-                    </div>
-                ))
-            ) : (
-                <div className="bg-slate-200 rounded-px mx-5 py-2 my-2 text-xl text-black">No Vaccine Booking</div>
-            )}
+            <div className="bg-slate-200 rounded-px mx-5 py-2 my-2 text-black" key={profile.data.id}>
+                <table className='table-auto border-separate border-spacing-2 bg-gray-100'>
+                    <tbody>
+                        <tr>
+                            <td>Name:</td>
+                            <td>{profile.data.name}</td>
+                        </tr>
+                        <tr>
+                            <td>Email:</td>
+                            <td>{profile.data.email}</td>
+                        </tr>
+                        <tr>
+                            <td>Tel.</td>
+                            <td>{profile.data.telephone}</td>
+                        </tr>
+                        <tr>
+                            <td>Member since:</td>
+                            <td>{formattedDate}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="bg-slate-200 rounded-px mx-5 py-2 my-2 text-xl text-black">No Reservation</div>
         </>
     );
 }
